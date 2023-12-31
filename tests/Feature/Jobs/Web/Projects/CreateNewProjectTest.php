4c94e7ba142dc72615bbd9a717eedc004dd218f2
DemianWHT <demiankost2@gmail.com>
2023-12-31 23:11:01 +0100

    Created test for CreateNewProject job

--- tests/Feature/Jobs/Web/Projects/CreateNewProjectTest.php
+++ tests/Feature/Jobs/Web/Projects/CreateNewProjectTest.php
@@ -1,3 +1,19 @@
 <?php
 
 declare(strict_types=1);
+
+use App\Models\Project;
+use App\Jobs\Web\Projects\CreateNewProject;
+
+it('it will create a new project', function(): void {
+    $this->expect(Project::query()->count())->toEqual(0);
+
+    $user = createUser(); 
+
+    dispatch(new CreateNewProject(
+        name: 'test',
+        team: $user->current_team_id
+    ));
+
+    $this->expect(Project::query()->count())->toEqual(1);
+})->group('projects');
