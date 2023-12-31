6b362e577ae22ef1fbea8a1def20dc8a0ae99f6a
DemianWHT <demiankost2@gmail.com>
2023-12-31 18:02:59 +0100

    New job for creating new project

--- app/Http/Controllers/Web/Projects/StoreController.php
+++ app/Http/Controllers/Web/Projects/StoreController.php
@@ -6,19 +6,27 @@ namespace App\Http\Controllers\Web\Projects;
 
 use App\Http\Controllers\Web\Projects\IndexController;
 use App\Http\Requests\Web\Projects\StoreRequest;
+use App\Jobs\Web\Projects\CreateNewProject;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Contracts\Auth\Factory;
 use JustSteveKing\Launchpad\Queue\DispatchableCommandBus;
 use function action;
 
 final class StoreController
 {
     public function __construct(
+        private readonly Factory $auth,
         private readonly DispatchableCommandBus $bus,
     ) {}
 
     public function __invoke(StoreRequest $request): RedirectResponse
     {
-
+        $this->bus->dispatch(
+            job: new CreateNewProject(
+                name: $request->string('name')->toString(),
+                team: $this->auth()->guard()->user()->current_team_id,
+            ),
+        );
 
         return new RedirectResponse(
             url: action(IndexController::class),
